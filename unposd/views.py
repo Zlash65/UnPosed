81789c7bb0ea8df50c431441a6a69a77deedf10a
Zlash65 <zarrar65@gmail.com>
2018-11-15 03:18:45 +0530

    registration page added

--- unposd/views.py
+++ unposd/views.py
@@ -1,9 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
 	return render(request, 'base.html')
+
+def logout_view(request):
+	logout(request)
+	return redirect('/unposd/login')
+
+def register(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/unposd')
+	else:
+		form = UserCreationForm()
+
+		args = {"form": form}
+		return render(request, 'login/reg_form.html', args)
