5564c8f360b1d5c7d9c0056b91a004bab1fa2095
Zlash65 <zarrar65@gmail.com>
2018-11-16 00:24:50 +0530

    view methods added for more end points

--- unposd/views.py
+++ unposd/views.py
@@ -9,7 +9,8 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
-	return render(request, 'base.html')
+	# return render(request, 'base.html')
+	return redirect('/unposd/photos')
 
 def logout_view(request):
 	logout(request)
@@ -26,3 +27,62 @@ def register(request):
 
 		args = {"form": form}
 		return render(request, 'login/reg_form.html', args)
+
+def groups(request):
+	from unposd.models import Groups
+	groups = list(Groups.objects.extra(where=["members like '%%%{0}%%%'"
+		.format(request.user.username)]))
+
+	args = {"data": groups}
+	return render(request, 'main/groups_list.html', args)
+
+def photos_list(request, group_id):
+	from unposd.models import Photos
+	photos = list(Photos.objects.filter(user=request.user.username).filter(group_id=group_id))
+
+	# tags_view = {}
+	# for d in photos:
+	# 	tags_view[d.photo_id] = len(d.tags.split(' | '))
+
+	args = {
+		"data": photos,
+		# "tags_view": tags_view
+	}
+	return render(request, 'main/photos_list.html', args)
+
+def photos_display(request, photo_id):
+	from unposd.models import Photos
+	photo = Photos.objects.filter(user=request.user.username).filter(photo_id=photo_id)[0]
+
+	args = {"photo": photo}
+	return render(request, 'main/photos_display.html', args)
+
+def photos(request):
+	from unposd.models import Photos
+	group_id = request.GET.get('group','')
+
+	if group_id:
+		photos = list(Photos.objects.filter(user=request.user.username).filter(group_id=group_id))
+
+		# tags_view = {}
+		# for d in photos:
+		# 	tags_view[d.photo_id] = len(d.tags.split(' | '))
+
+		args = {
+			"data": photos,
+			# "tags_view": tags_view
+		}
+		return render(request, 'main/photos_list.html', args)
+	else:
+		photos = list(Photos.objects.filter(user=request.user.username))
+
+		# tags_view = {}
+		# for d in photos:
+		# 	tags_view[d.photo_id] = len(d.tags.split(' | '))
+
+		args = {
+			"data": photos,
+			# "tags_view": tags_view
+		}
+		return render(request, 'main/photos_list.html', args)
+
